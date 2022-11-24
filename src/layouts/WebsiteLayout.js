f5ea96a3ec43aa9dafd2b861105278c01f8837eb
GuidoSeia <guidoseia5@gmail.com>
2022-11-23 23:27:22 -0300

    creacion de layout

---
+++ src/layouts/WebsiteLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
+
+const WebsiteLayout = () => {
+  return (
+    <>
+        <NavBar />
+            
+        <Footer />
+    </>
+  )
+}
+
+export default WebsiteLayout
