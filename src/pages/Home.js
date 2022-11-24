8c0291ac32456cdebab9bf11cde1ff86dea396de
GuidoSeia <guidoseia5@gmail.com>
2022-11-23 23:27:40 -0300

    nueva pagina home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Home.css"
+import NavBar from "../components/NavBar"
+
+
+function Home() {
+  return (
+    <div >
+        <NavBar/>
+    </div>
+  )
+}
+
+export default Home
