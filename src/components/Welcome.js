c51d6f188c5002aab2d93136e04de577c7f324c0
GuidoSeia <guidoseia5@gmail.com>
2022-11-25 14:25:35 -0300

    agregado de boton, slogan y modificaciones minimas de titulo y estilos

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,9 +6,11 @@ export default function Welcome() {
     <div className='home'>
         <div className='info'>
           <div className='container-info'>
-              <h2 className='title-home'>Epic</h2>
-              <h2 className='title-home'>Shoes</h2>
-
+              <h2 className='title-home w-full text-center mb-4'>Epic Shoes</h2>
+              <div className='flex items-center justify-center text-black'>              
+                <p className='w-96 text-center text-2xl'>«A new story for each shoe, designed for the freedom of walking»</p>            
+            </div>      
+            <input type="button" value="Shop Now !" className="btn mt-8 text-white" />   
           </div>
         </div>
         <div className='img'></div>
