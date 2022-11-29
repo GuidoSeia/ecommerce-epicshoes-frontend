15fa85cc815953a2170726648a0926367e04b2e8
Guido Seia <guidoseia5@gmail.com>
2022-11-28 23:55:15 -0300

    importaciond de componente carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "../styles/Home.css"
 import Welcome from "../components/Welcome"
+import Carousel from "../components/Carousel"
+
 
 
 function Home() {
   return (
     <div >
         <Welcome/>
+        <Carousel/>
     </div>
   )
 }
