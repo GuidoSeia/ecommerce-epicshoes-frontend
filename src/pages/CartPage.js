919304499d5b47f15114d3192e1a93fff059b28b
Guido Seia <guidoseia5@gmail.com>
2022-12-23 18:51:39 -0300

    pagina del carrito

---
+++ src/pages/CartPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from '../components/Cart'
+import Layout from '../layouts/Layout'
+
+
+export default function CartPage() {
+  return (
+    <div>
+        <Layout>
+            <Cart/>
+        </Layout>   
+    </div>
+  )
+}
